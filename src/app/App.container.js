a7f8d50c294af92cf576b156d4e96f9f396db52c
Tiago Assuncao <tiagoassunc@gmail.com>
2022-09-13 16:14:34 -0300

    Update 'App' component to use container

---
+++ src/app/App.container.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../styles/base/_base.scss";
+import { App as AppJsx } from "./App.component.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <AppJsx />
+    </div>
+  );
+};
+
+export default App;
