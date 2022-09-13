69ae575526054a274e944e84a0ae2e29180a2bc0
Tiago Assuncao <tiagoassunc@gmail.com>
2022-09-13 16:10:40 -0300

    Update 'App' component import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./app/App.component.jsx";
+
+import App from "./app/App.container";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
