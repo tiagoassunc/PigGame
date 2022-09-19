c5c9160b54cff4a95c7a5842b14376b4a3d82fe5
Tiago Assuncao <tiagoassunc@gmail.com>
2022-09-19 07:57:51 -0300

    Update App test to use of react router

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -3,15 +3,24 @@
  */
 
 import React from "react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import "@testing-library/jest-dom/extend-expect";
 import { render, cleanup, screen } from "@testing-library/react";
+
 import App from "./App.component.jsx";
 
 afterEach(cleanup);
 
 describe("This will test App", () => {
   test("renders message", () => {
-    render(<App />);
+    const history = createMemoryHistory();
+
+    render(
+      <Router location={history.location} navigator={history}>
+        <App />,
+      </Router>
+    );
     const app = screen.getByText("Pig Game");
     expect(app).toBeInTheDocument();
   });
