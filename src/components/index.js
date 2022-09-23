8114143ab718ef54d7f281308feea6d06a225053
Tiago Assuncao <tiagoassunc@gmail.com>
2022-09-23 11:23:08 -0300

    Update index components export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 import Header from "./Header/Header.container";
+import Game from "./Game/Game.container";
+import Player from "./Player/Player.container";
 
-export { Header };
+export { Header, Game, Player };
