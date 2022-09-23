f81547d49308693355884e8f6467d1a1a14924b7
Tiago Assuncao <tiagoassunc@gmail.com>
2022-09-23 10:03:07 -0300

    Update Pig page

--- src/pages/Pig.jsx
+++ src/pages/Pig.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Header } from "../components/index";
+import { Game } from "../components/index";
+
+import "../styles/layout/pig-page.scss";
 
 const PigGame = () => {
-  return <Header />;
+  return (
+    <div className="pigPageLayout">
+      <div className="pigPageLayout-header">
+        <Header />
+      </div>
+      <div className="pigPageLayout-content">
+        <Game />
+      </div>
+    </div>
+  );
 };
 
 export default PigGame;
