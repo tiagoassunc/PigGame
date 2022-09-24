8f97a4249b24935cec2b781ffa9db95543677561
Tiago Assuncao <tiagoassunc@gmail.com>
2022-09-24 00:37:38 -0300

    Update common components export

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header/Header.container";
 import Game from "./Game/Game.container";
 import Player from "./Player/Player.container";
+import Dice from "./Dice/Dice.container";
 
-export { Header, Game, Player };
+export { Header, Game, Player, Dice };
