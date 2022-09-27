23b7fdd88ed8c12f2884765e3ad484a4da278117
Tiago Assuncao <tiagoassunc@gmail.com>
2022-09-27 20:14:58 -0300

    Update pig page component

--- src/pages/Pig.jsx
+++ src/pages/Pig.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import { Header, Game } from "../components/index";
+import React, { useState } from "react";
+import { Header, Game, ConfigGame } from "../components/index";
 
 import "../styles/layout/pig-page.scss";
 
 const PigGame = () => {
+  const [isConfigActive, setIsConfigActive] = useState(true);
+
+  const configTab = () => {
+    if (isConfigActive === true) {
+      return (
+        <div className="pigPageLayout-config">
+          <ConfigGame />
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="pigPageLayout">
-      <div className="pigPageLayout-header">
-        <Header />
-      </div>
-      <div className="pigPageLayout-content">
-        <Game />
+      {configTab()}
+      <div className="pigPageMain pigPageLayout-main">
+        <div className="pigPageMain-header">
+          <Header />
+        </div>
+        <div className="pigPageMain-content">
+          <Game />
+        </div>
       </div>
     </div>
   );
